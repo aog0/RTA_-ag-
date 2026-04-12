28551a4433cb9e8a59f715492e58d6dbff9c265a
aog0 <ag121421@student.sgh.waw.pl>
2026-04-12 21:45:47 +0000

    PD_1

---
+++ consumer_pd.py
@@ -0,0 +1,34 @@
+from kafka import KafkaConsumer
+from collections import defaultdict
+from datetime import datetime, timedelta
+import json
+
+consumer = KafkaConsumer(
+    'transactions',
+    bootstrap_servers='broker:9092',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+    group_id='anomaly_detector'
+)
+
+user_time = defaultdict(list)
+
+print("Nasłuchuję na anomalie prędkości (> 3 transakcje w ciągu 60 sekund)...")
+
+for msg in consumer:
+    event = msg.value
+    user_id = event['user_id']
+
+    # Parsowanie daty
+    current_time = datetime.fromisoformat(event['timestamp'])
+
+    user_time[user_id].append(current_time)
+    
+    # 3. Zostawiamy w pamięci tylko te transakcje z ostatnich 60s
+    time_prog = current_time - timedelta(seconds=60)
+    user_time[user_id] = [t for t in user_time[user_id] if t >= time_prog]
+    
+    # 4. Sprawdzamy warunek (WIĘCEJ niż 3 transakcje)
+    tx_count = len(user_time[user_id])
+    if tx_count > 3:
+        print(f"Anomalia; użytkownik: {user_id} | "
+              f"Transakcje w ostatnich 60s: {tx_count}")
